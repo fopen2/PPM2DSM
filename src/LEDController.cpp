2b7ab9fab514919db183a49c86bc3b5db28d90d3
fopen2 <7832727+fopen2@users.noreply.github.com>
2019-01-31 15:48:01 +0100

    Re-added blinking when binding to the LEDController.
    The LEDController is no longer using a hardware timer.
    This was done because the timer that was used, timer0, is also used
    for millis() and micros(), which are used in the implementation
    of the waiting period for sending serial output.
    Additionally, the software-based implementation removes latency of the
    error led.

--- src/LEDController.cpp
+++ src/LEDController.cpp
@@ -9,8 +9,6 @@ LEDController::LEDController(){
 
 LEDController* cont;
 
-#undef USE_HARDWARE_TIMER
-
 void LEDController::begin(uint8_t pin){
 	this->pin = pin;
 	pinMode(pin, OUTPUT);
@@ -18,34 +16,23 @@ void LEDController::begin(uint8_t pin){
 	bindon=0;
   error=0;
 	cont=this;
-
-  #ifdef USE_HARDWARE_TIMER
-  TCCR0A = (1<<COM0A1)|(1<<WGM00);//phase correct pwm non-inverting mode. OCR0B disconnected.
+  /*
+  TCCR0A = (1<<WGM00);
   TCCR0B = (1<<CS00)|(1<<CS02);//1024 prescaler
-  OCR0A = 0;//led off
-  #endif
   //TIMSK0=0;*/
 }
 
 void LEDController::refresh(){
   if(error){
-    #ifdef USE_HARDWARE_TIMER
-    OCR0A=255;
-    #else
+    //TCCR0A = (1<<WGM00);
     digitalWrite(pin,HIGH);
-    #endif
   }else if(bindon){
-    #ifdef USE_HARDWARE_TIMER
-    OCR0A=127;
-    #else
-    digitalWrite(pin,LOW);
-    #endif
+    //TCCR0A = (1<<COM0A1)|(1<<WGM00);//phase correct pwm non-inverting mode. OCR0B disconnected.
+    //OCR0A = 127;//led off
+    digitalWrite(pin,millis()%2==0?HIGH:LOW);
   }else{
-    #ifdef USE_HARDWARE_TIMER
-    OCR0A=(ledswitches%2==0)?0:255;
-    #else
+    //TCCR0A = (1<<WGM00);
     digitalWrite(pin,(ledswitches%2==0)?LOW:HIGH);
-    #endif
   }
   if(ledswitches>0 && millis()>nextswitch){
     ledswitches--;
